5759b6ae21e6c6ebead0879097caea1fd2ca1178
Benjamin Leuzinger <beleuz@gmx.ch>
2025-03-16 12:18:50 +0100

    none ls nice

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -1,17 +1,19 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                -- null_ls.builtins.formatting.stylua,
-                -- null_ls.builtins.diagnostics.rubocop,
-                -- null_ls.builtins.formatting.rubocop,
-                -- null_ls.builtins.formatting.htmlbeautifier
-            }
-        })
+    null_ls.setup({
+      sources = {
+        -- null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.checkstyle.with({
+          extra_args = { "-c", "$ROOT/src/main/config/swc_checks.xml" }, -- or "/sun_checks.xml" or path to self written rules
+        }),
+        -- null_ls.builtins.formatting.checkstyle,
+        -- null_ls.builtins.formatting.htmlbeautifier
+      }
+    })
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end
 }
