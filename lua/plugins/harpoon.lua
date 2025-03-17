c09424381845daa679f671ecb09af8f28b75d54b
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-03-17 21:29:24 +0100

    lazy updating

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,6 @@ return {
         save_on_toggle = true
       }
     })
-    -- REQUIRED
 
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
