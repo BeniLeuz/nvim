2f823d64461b0af01e2c538966cee64984c32294
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-03-17 21:47:48 +0100

    rip harpoon fuck this shit

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,24 @@
 return {
-  "ThePrimeagen/harpoon",
-  requires = { {"nvim-lua/plenary.nvim"} },
-  config = function()
-    local harpoon = require("harpoon")
-
-    -- REQUIRED
-    harpoon:setup({
-      settings = {
-        save_on_toggle = true
-      }
-    })
-
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    vim.keymap.set("n", "<C-c>", function() harpoon.ui:close_menu() end)
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(5) end)
-    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(6) end)
-  end
+  -- "ThePrimeagen/harpoon",
+  -- requires = { {"nvim-lua/plenary.nvim"} },
+  -- config = function()
+  --   local harpoon = require("harpoon")
+  --
+  --   -- REQUIRED
+  --   harpoon:setup({
+  --     settings = {
+  --       save_on_toggle = true
+  --     }
+  --   })
+  --
+  --   vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+  --   vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+  --   vim.keymap.set("n", "<C-c>", function() harpoon.ui:close_menu() end)
+  --   vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+  --   vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+  --   vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+  --   vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+  --   vim.keymap.set("n", "<C-n>", function() harpoon:list():select(5) end)
+  --   vim.keymap.set("n", "<C-m>", function() harpoon:list():select(6) end)
+  -- end
 }
