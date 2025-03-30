b3522cee4e9dfb72c0e205e794f924e73e87c5ad
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-03-30 09:46:29 +0200

    whole lspconfig done with blink and default handler so most lsps are just setup out of the box now

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,96 +1,97 @@
 return {
   {
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"rubocop",
-					"ruby_lsp",
-					"ts_ls",
-					"cssls",
-					"solargraph",
-					"clangd",
-					"html",
-					"jdtls",
-					"hls",
-          "intelephense",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
     dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
       "saghen/blink.cmp"
     },
-		config = function()
-			local lspconfig = require("lspconfig")
-			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-
-      lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-      }
-
-			lspconfig.intelephense.setup({
-				capabilities = capabilities,
-			})
-
-      lspconfig.html.setup {
-        capabilities = capabilities,
-        filetypes = { 'html', 'md' },
-        settings = {
-          html = { format = { indentInnerHtml = true } }
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "ruby_lsp",
+          "ts_ls",
+          "html"
         }
-      }
+      })
 
-      lspconfig.hls.setup {
-        capabilities = capabilities,
-      }
+      local lspconfig = require("lspconfig")
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      lspconfig.clangd.setup {
-        capabilities = capabilities,
-        init_options = {
-          fallbackFlags = {
-            "-std=c++23",
-            "-I../include/",
-            "-I./include/",
+      require("mason-lspconfig").setup_handlers({
+        function(server_name) -- default handler (optional)
+          lspconfig[server_name].setup({
+            capabilities = capabilities
+          })
+        end,
+        ["clangd"] = function()
+          lspconfig.clangd.setup {
+            capabilities = capabilities,
+            init_options = {
+              fallbackFlags = {
+                "-std=c++23",
+                "-I../include/",
+                "-I./include/",
+              }
+            }
           }
-        }
-      }
-
-      lspconfig.ruby_lsp.setup {
-        capabilities = capabilities,
-        filetypes = { 'ruby' },
-      }
-
-      lspconfig.solargraph.setup {
-        capabilities = capabilities,
-        filetypes = { 'ruby', 'eruby' },
-      }
-
-      lspconfig.jdtls.setup {
-        capabilities = capabilities,
-      }
-
-      -- not actually needed cause solargraph crazy fr
-      --      lspconfig.rubocop.setup {
-      --        capabilities = capabilities,
-      --      }
-
-      lspconfig.ts_ls.setup {
-        capabilities = capabilities,
-      }
-
-      lspconfig.cssls.setup {
-        capabilities = capabilities,
-      }
+        end,
+        ["html"] = function()
+          lspconfig.html.setup {
+            capabilities = capabilities,
+            filetypes = { 'html', 'md' },
+            settings = {
+              html = { format = { indentInnerHtml = true } }
+            }
+          }
+        end,
+        -- old configs if i need again
+        --
+        --
+        -- lspconfig.lua_ls.setup {
+        --   capabilities = capabilities,
+        -- }
+        --
+        -- lspconfig.intelephense.setup({
+        --   capabilities = capabilities,
+        -- })
+        --
+        --
+        -- lspconfig.hls.setup {
+        --   capabilities = capabilities,
+        -- }
+        --
+        --
+        -- lspconfig.ruby_lsp.setup {
+        --   capabilities = capabilities,
+        --   filetypes = { 'ruby' },
+        -- }
+        --
+        -- lspconfig.solargraph.setup {
+        --   capabilities = capabilities,
+        --   filetypes = { 'ruby', 'eruby' },
+        -- }
+        --
+        -- lspconfig.jdtls.setup {
+        --   capabilities = capabilities,
+        -- }
+        --
+        -- -- not actually needed cause solargraph crazy fr
+        -- --      lspconfig.rubocop.setup {
+        -- --        capabilities = capabilities,
+        -- --      }
+        --
+        -- lspconfig.ts_ls.setup {
+        --   capabilities = capabilities,
+        -- }
+        --
+        -- lspconfig.cssls.setup {
+        --   capabilities = capabilities,
+        -- }
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
