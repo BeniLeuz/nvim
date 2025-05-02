c15255a31ba3fd04d716b8b5d21be2d0f5cfc6c1
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-05-02 22:40:46 +0200

    remove dirty little autocomplete q: floating line window

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -40,7 +40,33 @@ return {
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
       providers = {
+        cmdline = {
+          -- ignores cmdline completions when executing shell commands
+          enabled = function()
+            return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+          end
+        },
+        buffer = {
+          enabled = function()
+            local buftype = vim.bo.buftype
+
+            if buftype == 'prompt' or buftype == 'nofile' then
+              return false
+            end
+
+            return true
+          end
+        },
         snippets = {
+          enabled = function()
+            local buftype = vim.bo.buftype
+
+            if buftype == 'prompt' or buftype == 'nofile' then
+              return false
+            end
+
+            return true
+          end,
           opts = {
             extended_filetypes = {
               svelte = { "html" },
