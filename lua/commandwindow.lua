ee002ee0cec3f1b36d5c6c22d26c0fc83b1fdf76
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-05-03 13:08:01 +0200

    pog this work like a charm rn only thing missing is history from normal command line remove and maybe opening it standardly

--- lua/commandwindow.lua
+++ lua/commandwindow.lua
@@ -4,13 +4,6 @@ function executeModifiedCommand()
   -- Get the current line (command)
   local cmd = vim.fn.getline(".")
 
-  -- Modify the command (example: add a comment)
-  local modified_cmd = "new tmp | r " .. cmd
-
-  -- For debugging
-  print("Original command: " .. cmd)
-  print("Modified command: " .. modified_cmd)
-
   -- Exit insert mode and quit the command-line window
   vim.api.nvim_feedkeys(
     vim.api.nvim_replace_termcodes("<Esc>:quit<CR>", true, false, true),
@@ -18,29 +11,32 @@ function executeModifiedCommand()
     false
   )
 
-
   -- Schedule command execution after window closes
   vim.schedule(function()
-    -- If the previous buffer exists, wipe it
-    if M.previous_buffer then
-      vim.api.nvim_buf_delete(M.previous_buffer, { force = true }) -- Delete the previous buffer
+    if cmd:sub(1, 1) == "!" then
+      -- If the previous buffer exists, wipe it
+      if M.previous_buffer then
+        vim.api.nvim_buf_delete(M.previous_buffer, { force = true }) -- Delete the previous buffer
+      end
+
+      -- Run the shell command and capture the output if it is ! command
+      local output = vim.fn.system(cmd:sub(2))
+
+      vim.cmd("enew")
+      local buf = vim.api.nvim_get_current_buf()
+
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(output, "\n"))
+      vim.api.nvim_buf_set_name(0, "command://output")
+      vim.cmd("setlocal buftype=nofile") -- Treat the buffer as 'nofile'
+      vim.cmd("setlocal filetype=projterm")
+      -- set current buffer as previous buffer
+      M.previous_buffer = buf
+
+      -- add command to history lol
+      vim.fn.histadd(":", cmd)
+    else
+      vim.cmd(cmd)
     end
-
-    -- Run the shell command and capture the output if it is ! command
-    local output = vim.fn.system(cmd)
-
-    vim.cmd("enew")
-    local buf = vim.api.nvim_get_current_buf()
-
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(output, "\n"))
-    vim.api.nvim_buf_set_name(0, "command://output")
-    vim.cmd("setlocal buftype=nofile") -- Treat the buffer as 'nofile'
-    vim.cmd("setlocal filetype=projterm")
-    -- set current buffer as previous buffer
-    M.previous_buffer = buf
-
-    -- add command to history lol
-    vim.fn.histadd(":", cmd)
   end)
 end
 
