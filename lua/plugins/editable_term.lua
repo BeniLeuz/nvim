de4c97f0dacb967b59a5e62467307ecb211561ac
Benjamin Leuzinger <beleuz@gmx.ch>
2025-05-04 18:35:09 +0200

    should work for macos and rest as well now

--- lua/plugins/editable_term.lua
+++ lua/plugins/editable_term.lua
@@ -1,11 +1,10 @@
 return {
-  'xb-bx/editable-term.nvim',
+  'BeniLeuz/editable-term.nvim',
   config = function()
-    -- this might be the best plugin i have ever seen in my entire life, neovim, plesae upstream this
     local editableterm = require('editable-term')
     editableterm.setup({
       promts = {
-        ['.*[$#][ ]?'] = {}
+        ['.*[$#%%][ ]?'] = {}
       },
     })
   end,
