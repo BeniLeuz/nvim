d4e7fb3ec5520dffefc841d8b5c3b118f55a5a6c
Benjamin Leuzinger <benjamin.leuzinger@students.fhnw.ch>
2025-09-26 03:36:54 +0200

    nice rebase -i shnould work now pog

--- lua/projectionizer.lua
+++ lua/projectionizer.lua
@@ -1,46 +1,53 @@
+local function term_intercept_cr()
+  local line = vim.api.nvim_get_current_line()
+
+  local s, e = line:find("nvim ")
+
+  if not s then
+    vim.env.CLOSE_UNNEST = 0
+  else
+    vim.env.CLOSE_UNNEST = 1
+  end
+
+  return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+end
+
+vim.keymap.set("t", "<CR>", term_intercept_cr, { expr = true, noremap = true })
+
 if not vim.env.NVIM then
-	return
+  return
 end
 
 local _, parent_chan = pcall(vim.fn.sockconnect, "pipe", vim.env.NVIM, { rpc = true })
 
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
+  callback = function()
     vim.rpcrequest(parent_chan, "nvim_command", "tabnew " .. vim.api.nvim_buf_get_name(0))
     vim.rpcrequest(parent_chan, "nvim_command", "tcd " .. vim.fn.getcwd())
-    vim.cmd(":q!")
+
+    local close = vim.rpcrequest(parent_chan, "nvim_eval", 'getenv("CLOSE_UNNEST")')
+    if close == "1" then
+      -- needs to be done since if we ever do nvim and then into nvim . it might still try to use the old ones so clear them!
+      vim.rpcrequest(parent_chan, "nvim_command", "augroup UnnestAutoClose | autocmd! | augroup END")
+      vim.cmd(":q!")
+      return
+    end
+
+    -- create/clear the group first
+    vim.rpcrequest(parent_chan, "nvim_command", "augroup UnnestAutoClose | autocmd! | augroup END")
+
+    -- add the new TabClosed autocmd
+    local tabpagenr = vim.rpcrequest(parent_chan, "nvim_call_function", "tabpagenr", {})
+    local child_server = vim.v.servername
+
+    local cmd = string.format([[
+augroup UnnestAutoClose
+  autocmd TabClosed * if expand("<afile>") == "%d"
+    call rpcnotify(sockconnect('pipe', '%s', #{ rpc: v:true }), 'nvim_command', 'quitall!')
+  endif
+augroup END
+]], tabpagenr, child_server)
+
+    vim.rpcrequest(parent_chan, "nvim_command", cmd)
   end
 })
-
--- if i EVER want to have git rebase -i working too while having my implementation of nvim unnest
--- with unnest.nvim this stuff needs to be done setuff and sso one basldkbfalshfdsalkjfhsd
--- local function term_intercept_cr()
---   local line = vim.api.nvim_get_current_line()
---
---   -- open inside new project only if found with a space. I only open projects with nvim .
---   local s, e = line:find("nvim ")
---
---   if not s then
---     return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
---   end
---
---   -- extract the argument after "nvim"
---   local arg = line:sub(e + 1):gsub("^%s+", "") -- e.g., "." or "../file"
---
---   -- get terminal cwd
---   -- TODO: this does not work yet lol path is f'd up
---   local bufnr = vim.api.nvim_get_current_buf()
---   local term_name = vim.api.nvim_buf_get_name(bufnr)
---   local cwd = term_name:match("^term://([^/]+)//") or vim.loop.cwd()
---   local full_path = vim.fn.fnamemodify(cwd .. "/" .. arg, ":p")
---
---   vim.schedule(function()
---     vim.cmd("tabnew " .. full_path)
---   end)
---
---   -- clear the line after opening new project :)
---   -- does this even happen after or will it just fucking send control c to the tabnew? i think it will lol
---   return vim.api.nvim_replace_termcodes("<C-C>", true, false, true)
--- end
-
--- vim.keymap.set("t", "<CR>", term_intercept_cr, { expr = true, noremap = true })
