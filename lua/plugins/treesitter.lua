a83a568fda90a95a8a05184063100b3307e8e50f
Benjamin Leuzinger <beleuz@gmx.ch>
2025-10-01 15:59:04 +0200

    make sure lhs is treated as normal haskell treesitter is sus

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,15 @@
 local configs = require("nvim-treesitter.configs")
 
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.lhs",
+  callback = function()
+    vim.bo.filetype = "haskell"
+  end,
+})
+
 configs.setup({
   highlight = { enable = true },
   indent = { enable = true },
   auto_install = true,
 })
+
