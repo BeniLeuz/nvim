007fdc4a276e2735fc737726c8218258d39c1be1
Benjamin Leuzinger <beleuz@gmx.ch>
2025-10-13 21:27:41 +0200

    plain around but lets see if i like a git plugin or sumn

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 vim.pack.add({
   { src = "https://github.com/habamax/vim-polar" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/ThePrimeagen/harpoon",                     version = "harpoon2" },
+  { src = "https://github.com/ThePrimeagen/harpoon",                    version = "harpoon2" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
-  { src = "https://github.com/Saghen/blink.cmp",                         version = "v1.0.0" },
+  { src = "https://github.com/Saghen/blink.cmp",                        version = "v1.0.0" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
   { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/chomosuke/term-edit.nvim", version = "v1.4.0" },
+  { src = "https://github.com/chomosuke/term-edit.nvim",                version = "v1.4.0" },
   -- neotest
   -- INFO: needs to be ran to download junit console
   -- :NeotestJava setup
@@ -42,7 +42,6 @@ vim.pack.add({
 -- [difftool "nvim_difftool"]
 --     cmd = nvim -c \"set diff\" -c \"silent! DiffTool $LOCAL $REMOTE\"
 vim.cmd("packadd nvim.difftool")
-
 require("vim-options")
 require("vim-remaps")
 require("theme")
@@ -67,3 +66,13 @@ require("plugins.neotest")
 
 -- for printing
 -- vim.cmd("colorscheme polar");
+-- vim.keymap.set('n', '<leader>di', function()
+--   local remote_branch = vim.fn.system('git rev-parse --abbrev-ref @{upstream} 2>/dev/null'):gsub('\n', '')
+--   if remote_branch == '' then
+--     vim.notify('No upstream branch set', vim.log.levels.WARN)
+--     return
+--   end
+--   local chan = vim.fn.termopen(vim.o.shell)
+--   -- Send the git difftool command
+--   vim.fn.chansend(chan, 'git d -d ' .. remote_branch .. '\n')
+-- end, { desc = 'Git difftool vs upstream' })
