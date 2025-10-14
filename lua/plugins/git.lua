250d84df57f60544e1b5588714239fbf66f6585d
Benjamin Leuzinger <beleuz@gmx.ch>
2025-10-14 02:36:32 +0200

    ok three keybinds seems valid

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,17 +9,14 @@
 -- cc = commit
 -- X = delete change
 
---checking
+-- overview
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
--- git diff all and git diff head all
-vim.api.nvim_create_autocmd("FileType", {
-  callback = function()
-    vim.keymap.set("n", "<leader>da", ":Git difftool -y<CR>", { buffer = true, noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>dh", ":Git difftool -y HEAD<CR>", { buffer = true, noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>du', function()
-      local branch = vim.fn.FugitiveHead()
-      vim.cmd('Git difftool -y ' .. '@{upstream}..' .. branch)
-    end, { desc = 'Git diff against upstream' })
-  end,
-})
+-- diff unstaged against working dir
+vim.keymap.set("n", "<leader>da", ":Git difftool -y<CR>")
+-- diff staged against head
+vim.keymap.set("n", "<leader>dh", ":Git difftool -y HEAD<CR>")
+-- diff against upstream
+vim.keymap.set('n', '<leader>du', function()
+  vim.cmd('Git difftool -y ' .. '@{upstream}..' .. vim.fn.FugitiveHead())
+end, { desc = 'Git diff against upstream' })
