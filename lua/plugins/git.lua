e0037647ec8c38df711de5e75f2a29a7938c97be
Benjamin Leuzinger <beleuz@gmx.ch>
2025-10-16 00:40:09 +0200

    i now have insane quickfix list for fugitive lol that was not worth it XD

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,5 +15,71 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 -- vim.keymap.set("n", "<leader>da", ":Git difftool -y<CR>")
 -- diff staged against head
 -- vim.keymap.set("n", "<leader>dh", ":Git difftool -y HEAD<CR>")
--- diff all against upstream
-vim.keymap.set('n', '<leader>du', ':Git difftool -y @{upstream}<CR>')
+-- diff all against upstream in TABS
+-- vim.keymap.set('n', '<leader>du', ':Git difftool -y @{upstream}<CR>')
+
+local function open_qf_diff()
+  vim.cmd('.cc')
+  vim.cmd('Gvdiffsplit @{upstream}')
+  vim.cmd('copen 10')
+  vim.cmd('wincmd k')
+  vim.cmd('wincmd l')
+end
+
+-- diff with a double view and quickfix
+vim.keymap.set('n', '<leader>du', function()
+  vim.cmd('Git difftool @{upstream}')
+  open_qf_diff()
+end)
+
+
+-- just so we dont reset on subsequent autocmds
+local qf_mapping_set = false
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function(args)
+    if qf_mapping_set then return end
+    qf_mapping_set = true
+
+    vim.keymap.set('n', '<CR>', function()
+      local qf_list = vim.fn.getqflist({ title = 0 })       -- Get current list each time
+
+      if not (qf_list.title and string.match(qf_list.title, 'difftool @')) then
+        vim.cmd('.cc')
+        return
+      end
+
+      pcall(function()
+        vim.cmd('windo if &filetype != "qf" | execute "close" | endif')
+        open_qf_diff()
+      end)
+    end, { buffer = args.buf })
+  end
+})
+
+vim.keymap.set('n', '[q', function()
+  local qf_list = vim.fn.getqflist({ title = 0 })
+  if qf_list.title and string.match(qf_list.title, 'difftool @') then
+    local _, _ = pcall(function()
+      vim.cmd('cprevious')
+      vim.cmd('windo if &filetype != "qf" | execute "close" | endif')
+      open_qf_diff()
+    end)
+  else
+    pcall(vim.cmd, 'cprevious')
+  end
+end)
+
+vim.keymap.set('n', ']q', function()
+  local qf_list = vim.fn.getqflist({ title = 0 })
+  if qf_list.title and string.match(qf_list.title, 'difftool @') then
+    local ok, err = pcall(function()
+      vim.cmd('cnext')
+      vim.cmd('windo if &filetype != "qf" | execute "close" | endif')
+      open_qf_diff()
+    end)
+  else
+    pcall(vim.cmd, 'cnext')
+  end
+end)
